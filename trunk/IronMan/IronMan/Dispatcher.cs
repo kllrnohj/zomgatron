1aa61878493d97dfc6f187bdecd2f999c79b517e
Kivu.Rako <Kivu.Rako@54144a58-e977-11dd-a550-c997509ed985>
2009-01-24 16:38:36 +0000

    +Dispatcher start commit

---
+++ trunk/IronMan/IronMan/Dispatcher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AgentsAPI;
+
+namespace IronMan
+{
+    static class Dispatcher
+    {
+        static int HRateAgent(PhoneCallEvent call, Agent agent)
+        {
+            return -1;
+        }
+
+        static void RouteCall(PhoneCallEvent call, List<Agent> agents)
+        {
+            foreach (Agent agent in agents)
+            {
+                
+            }
+        }
+    }
+}
