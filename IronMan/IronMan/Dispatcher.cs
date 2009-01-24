205d4ca703be5929d19eb01d497e8a46e1585254
Kivu.Rako <Kivu.Rako@54144a58-e977-11dd-a550-c997509ed985>
2009-01-24 17:11:38 +0000

    +sexified that shizit

--- IronMan/IronMan/Dispatcher.cs
+++ IronMan/IronMan/Dispatcher.cs
@@ -6,11 +6,37 @@ using AgentsAPI;
 
 namespace IronMan
 {
-    class AgentNotFoundException : Exception { }
-
     static class Dispatcher
     {
-        static int HEnumToHInt(ProficiencyLevel pl)
+        static private List<PhoneCallEvent> calls = new List<PhoneCallEvent>();
+        static private List<Agent> availableAgents = new List<Agent>();
+
+        static public bool NeedsProcessing()
+        {
+            return (calls.Count > 0 && availableAgents.Count > 0);
+        }
+
+        static public void AddPhoneCall(PhoneCallEvent call)
+        {
+            calls.Add(call);
+        }
+
+        static public void AddAvailableAgent(Agent available)
+        {
+            availableAgents.Add(available);
+        }
+
+        static public void ProcessQueue()
+        {
+            int bestCallVal = 0;
+            int bestCallIndex = -1;
+            for(int x = 0; x < calls.Count; x++)
+            {
+                int thisCallChoice;
+            }
+        }
+
+        static public int HEnumToHInt(ProficiencyLevel pl)
         {
             switch (pl)
             {
@@ -27,35 +53,37 @@ namespace IronMan
                     return 1;
             }
         }
-        static int HRateAgent(PhoneCallEvent call, Agent agent)
+        static public int HRateAgent(int callIndex, int agentIndex)
         {
-            int awesomeness = (agent.AgentStatusType == AgentStatusType.Unavailable || agent.AgentStatusType == AgentStatusType.OnCall) ? 0 : 1;
+            int awesomeness = (availableAgents[agentIndex].AgentStatusType == AgentStatusType.Unavailable || availableAgents[agentIndex].AgentStatusType == AgentStatusType.OnCall) ? 0 : 1;
 
-            foreach (Skill s in call.SkillsNeeded)
+            foreach (Skill s in calls[callIndex].SkillsNeeded)
             {
-                if(agent.Skills.Contains(s)){
-                    awesomeness *= HEnumToHInt(agent.Skills[agent.Skills.IndexOf(s)].ProficiencyLevel);
+                if(availableAgents[agentIndex].Skills.Contains(s)){
+                    awesomeness *= HEnumToHInt(availableAgents[agentIndex].Skills[availableAgents[agentIndex].Skills.IndexOf(s)].ProficiencyLevel);
                 }
             }
 
             return awesomeness;
         }
 
-        static void RouteCall(CallCenter callcenter, PhoneCallEvent call, List<Agent> agents)
+        static public Agent RouteCall(CallCenter callcenter, int callIndex, out int agentStrength)
         {
             int highest = 0;
             Agent best = null;
-            foreach (Agent agent in agents)
+            for(int x = 0; x < availableAgents.Count; x++)
             {
-                if (HRateAgent(call, agent) > highest || best == null)
-                    best = agent;
+                int val = HRateAgent(callIndex, x); 
+                if (val > highest || best == null)
+                {
+                    best = availableAgents[x];
+                    highest = val;
+                }
 
             }
 
-            if (best == null)
-                throw new AgentNotFoundException("Could not find appropriate Agent for call routing.");
-
-            callcenter.TransferCall(call.PhoneCallID, best.AgentID);
+            agentStrength = highest;
+            return best;
         }
     }
 }
