44d6ee1c5f98b8a5012decafd390e5d0a4d0dd3d
Kivu.Rako <Kivu.Rako@54144a58-e977-11dd-a550-c997509ed985>
2009-01-24 18:27:59 +0000

    + do you wanna die? I'll promise you

--- trunk/IronMan/IronMan/Visualizer.cs
+++ trunk/IronMan/IronMan/Visualizer.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Drawing;
 namespace IronMan
 {
     public partial class NUIronManForm : Form
     {
         protected override void OnPaint(PaintEventArgs e)
         {
+            Graphics g = e.Graphics;
+            Pen pen = (Pen)new Pen(Color.Green, 1.0f).Clone();
+            g.Clear(Color.Black);
+            for (int x = 7; x > 3; x--)
+            {
+                pen.Color = Color.FromArgb(255 - ((x-4) * 0), Color.Green);
+                Rectangle ellipse = new Rectangle(30, 30, x, x);
+                g.DrawEllipse(pen, ellipse);
+            }
             base.OnPaint(e);
         }
     }
