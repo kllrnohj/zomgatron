a79322976ee2faf5aea12a6b62804161edaec3e0
Kivu.Rako <Kivu.Rako@54144a58-e977-11dd-a550-c997509ed985>
2009-01-24 16:42:36 +0000

    +method signature changed

--- IronMan/IronMan/Dispatcher.cs
+++ IronMan/IronMan/Dispatcher.cs
@@ -6,19 +6,30 @@ using AgentsAPI;
 
 namespace IronMan
 {
+    class AgentNotFoundException : Exception { }
+
     static class Dispatcher
     {
         static int HRateAgent(PhoneCallEvent call, Agent agent)
         {
-            return -1;
+
         }
 
-        static void RouteCall(PhoneCallEvent call, List<Agent> agents)
+        static void RouteCall(CallCenter callcenter, PhoneCallEvent call, List<Agent> agents)
         {
+            int highest = 0;
+            Agent best = null;
             foreach (Agent agent in agents)
             {
-                
+                if (HRateAgent(call, agent) > highest || best == null)
+                    best = agent;
+
             }
+
+            if (best == null)
+                throw new AgentNotFoundException("Could not find appropriate Agent for call routing.");
+
+            callcenter.TransferCall(call.PhoneCallID, best.AgentID);
         }
     }
 }
